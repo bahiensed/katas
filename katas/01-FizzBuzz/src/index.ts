591a09b85a4f2b97e87a4f3bbddbb9e2cf12c8e4
Douglas Bahiense <douglas@rohling.com.br>
2024-11-17 16:36:27 -0300

    feat: create FizzBuzz convert function

---
+++ katas/01-FizzBuzz/src/index.ts
@@ -0,0 +1,10 @@
+const convert = (number: number): string => {
+  if (number === 0) return '0';
+  // if (number % 3 === 0 && number % 5 === 0) return 'FizzBuzz';
+  if (number % 15 === 0) return 'FizzBuzz';
+  if (number % 5 === 0) return 'Buzz';
+  if (number % 3 === 0) return 'Fizz';
+  return number.toString();
+};
+
+export { convert };
